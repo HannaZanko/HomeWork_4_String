8807ca8f594ce6a271c79739b4c0cfcaf1ee612f
ANYA <anya.aksenova1011@gmail.com>
2021-06-26 00:12:33 +0300

    4st commit

--- src/Task_12.java
+++ src/Task_12.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_12 {
     public static void main(String[] args) {
         System.out.println("Введите текст: ");
         String str = new Scanner(System.in).next();
-        str = str.replaceAll("[;:]-*[()\\[\\]]+}", "$");
+
+        str = str.replaceAll("[;:]-*[()\\[\\]]{1,}", String.valueOf('&'));
         int countSmile = 0;
         for (char c : str.toCharArray()) {
-            if (c == '$') {
+            if (c == '&') {
                 countSmile++;
             }
         }

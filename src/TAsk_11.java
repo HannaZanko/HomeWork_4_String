46d0b6f5fc6d7b8b8c7391133a99d301345dd406
ANYA <anya.aksenova1011@gmail.com>
2021-06-26 00:00:29 +0300

    3st commit

--- src/TAsk_11.java
+++ src/TAsk_11.java
@@ -1,28 +1,23 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TAsk_11 {
+
+    private static String word;
+
     public static void main(String[] args) {
 
-        System.out.println("Введите слово: ");
-        String word = new Scanner(System.in).nextLine();
-        char[] arrWord = word.toLowerCase().toCharArray();
-        int k = 0;
-        for (int i = 0; i < arrWord.length; i++) {
-            if ((arrWord[i] == 'a' || arrWord[i] == 'b' || arrWord[i] == 'c') && (!word.contains("bb"))) {
-                if (word.contains("[a-c]")) {
+        word = new Scanner(System.in).nextLine().toLowerCase();
 
-                }
-
-            } else {
-                System.out.println("Слово не существует");
-                break;
-            }
+        if (isRegexIn("[d-z]") || isRegexIn("(.)\\1\\1") || isRegexIn("[b][b]") || isRegexIn("([a-c][a-c])\\1\\1")
+                || isRegexIn("([a-c][a-c][a-c])\\1\\1") || isRegexIn("([a-c][a-c][a-c][a-c])\\1\\1")) {
+            System.out.println("Word is not Mumba-Umba");
+        } else {
+            System.out.println("Word is Mumba-Umba");
         }
-        if (arrWord.length == k) {
-            System.out.printf("Слово %s принадлежит языку Мумба-Юмба", word);
-
-        }
-
     }
+
+    private static boolean isRegexIn(String regex) {
+        return !(word.replaceFirst(regex, "").length() == word.length());
+    }
+
 }
--- src/Task_12.java
+++ src/Task_12.java
@@ -4,27 +4,12 @@ public class Task_12 {
     public static void main(String[] args) {
         System.out.println("Введите текст: ");
         String str = new Scanner(System.in).next();
-        char[] charStr = str.toCharArray();
+        str = str.replaceAll("[;:]-*[()\\[\\]]+}", "$");
         int countSmile = 0;
-        int j = 0;
-       // int count = 0;
-        while (j < charStr.length - 1) {
-         //   count = j;
-            if (charStr[j] == ':' || charStr[j] == ';') {
-                j++;
-                while (charStr[j] == '-') {
-                    j++;
-                }
-                while (charStr[j] == ')' || charStr[j] == '(' || charStr[j] == ']' || charStr[j] == '[') {
-                    j++;
-                    if (charStr[j] != ')' && charStr[j] != '(' && charStr[j] != ']' && charStr[j] != '[') {
-                        countSmile++;
-                    }
-                }
-
-
+        for (char c : str.toCharArray()) {
+            if (c == '$') {
+                countSmile++;
             }
-            j++;
         }
         System.out.println(countSmile);
     }
